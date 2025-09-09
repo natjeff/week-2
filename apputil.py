ebcacfb25874ea927b0dbe70624aa61f7245b695
natjeff <natjeff@iu.edu>
2025-09-09 15:30:37 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,13 +1,32 @@
 import numpy as np
 
-
 # update/add code below ...
 
-def ways(n):
-    return None
+#Function to calculate number of ways
+def ways(n: int) -> int:
+    return n // 5 + 1
 
+#Test Cases Below
+print(ways(12))
+print(ways(20))
+print(ways(3))
+print(ways(0))
+
+# Find lowest score
 def lowest_score(names, scores):
-    return None
+    idx = np.argmin(scores)
+    return names[idx]
 
+# Sort names by score
 def sort_names(names, scores):
-    return None
+    idx_sorted = np.argsort(scores)[::-1]
+    sorted_names = names[idx_sorted]
+    sorted_scores = scores[idx_sorted]
+    return list(zip(sorted_names, sorted_scores))
+
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
+
+#Test Cases Below
+print(lowest_score(names, scores))
+print(sort_names(names, scores))
